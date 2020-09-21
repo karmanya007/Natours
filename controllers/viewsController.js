398d824cf2ba7f1d4d03f9938e6da8efb0e7fcb7
Karmanya Veer Sharma <karmanyasharma5@gmail.com>
2020-09-21 23:30:24 +0530

    Stripe alerts

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,7 +4,7 @@ const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
-exports.alerts = (req, res, next) => {
+exports.alert = (req, res, next) => {
 	const { alert } = req.query;
 	if (alert === 'booking')
 		res.locals.alert =
